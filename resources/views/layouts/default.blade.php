f6baa56ebaf5c519d54f1ddd27420f94b64a4c31
Test <test@test.cn>
2019-12-10 15:19:19 +0800

    set header and footer subview

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,22 +8,12 @@
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">Weibo APP</a>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link" href="/help">Help</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    @include('layouts._header')
 
     <div class="container">
         @yield('content')
+
+        @include('layouts._footer')
     </div>
 </body>
 </html>
