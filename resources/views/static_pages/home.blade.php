daeac4317e4232d2222c350ba2715c6a29f5f51f
Test <test@test.cn>
2019-12-11 12:38:01 +0800

    user register page

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,7 +6,7 @@
         <p class="lead">Now you see <a href="./">Index</a></p>
         <p>Start here</p>
         <p>
-            <a class="btn btn-lg btn-success" href="#" role="button">Register</a>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Register</a>
         </p>
     </div>
 @endsection
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+    <h1>Register</h1>
+@endsection
