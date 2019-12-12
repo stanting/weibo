71775d82e6e865de60e392f99dbf48ca6fe9877d
Test <test@test.cn>
2019-12-12 11:59:40 +0800

    User register form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.default')
 
+@section('title', 'Register')
+
 @section('content')
-    <h1>Register</h1>
+<div class="offset-md-2 col-md-8">
+    <div class="card">
+        <div class="card-header">
+            <h5>Register</h5>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('users.store') }}" method="POST">
+                <div class="form-group">
+                  <label for="name">Name:</label>
+                  <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                </div>
+                <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                </div>
+                <div class="form-group">
+                  <label for="password">Password:</Password:></label>
+                  <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                </div>
+                <div class="form-group">
+                  <label for="password_confirmation">Password confirmation:</label>
+                  <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
