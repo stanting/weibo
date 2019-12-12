4c2fe90bf1ecda50c211e64416068c412e0f75ee
stanting <stanting@163.com>
2019-12-12 16:42:37 +0800

    Create session

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', 'Welcom back.');
+            return redirect()->route('users.show', Auth::user()->id);
+        } else {
+            session()->flash('danger', 'Sorry, incorrect email or password.');
+            return redirect()->back()->withInput();
+        }
+
+    }
+}
---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.default')
+
+@section('title', 'Login')
+
+@section('content')
+    <div class="offset-md-2 col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5>Login</h5>
+            </div>
+            <div class="card-body">
+                @include('shared._errors')
+
+                <form action=""{{ route('login') }} method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                      <label for="email">Email:</label>
+                      <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="password">Password:</label>
+                      <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+                <hr>
+                <p>Don’t have an account? <a href="{{ route('signup') }}">Sign up</a></p>
+            </div>
+        </div>
+    </div>
+@endsection
