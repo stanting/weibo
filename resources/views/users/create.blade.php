ecab1b3514fbde4b210f017cfd8d85e4037a7d1d
Test <test@test.cn>
2019-12-12 13:52:23 +0800

    Error prompt in Chinese

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,9 @@
             <h5>Register</h5>
         </div>
         <div class="card-body">
+
+            @include('shared._errors')
+
             <form action="{{ route('users.store') }}" method="POST">
                 {{ csrf_field() }}
 
